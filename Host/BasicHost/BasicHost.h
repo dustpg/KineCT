6ece697a8a6bd24a4e0e73f6535c0f8f0d761ace
Dust Loong <dustpg@gmail.com>
2015-05-30 15:37:52 +0800

    basic host added

---
+++ Host/BasicHost/BasicHost.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "../../KineCT/KinectCT.h"
+
+
+// Kinect V1, Do not support
+#define USING_KINECT_V1
+// Kinect V2
+#define USING_KINECT_V2
+
+
+#ifdef USING_KINECT_V2
+#include <Kinect.h>
+#endif
+
+
+// namespace 
+namespace KineCT {
+    // Basic Host 
+    class alignas(sizeof(void*)) CCTBasicHost final : public ICTHost {
+    public: // impl ICTInterface
+        // release the interface
+        void Release() noexcept override;
+    public: // impl ICTHost
+        // initialize
+        auto Initialize() noexcept->HRESULT;
+        // set media type
+        virtual auto SetMediaType(const CTMEDIATYPE&) noexcept->HRESULT;
+        // fill the buffer
+        virtual auto FillBuffer(uint8_t* buffer, size_t length) noexcept->HRESULT;
+    public:
+        // ctor
+        CCTBasicHost(ICTServer*) noexcept;
+    private:
+        // ctor
+        ~CCTBasicHost() noexcept;
+    private:
+        // server
+        ICTServer*                  m_pServer = nullptr;
+        // current processor
+        ICTProcessor*               m_pProcessor = nullptr;
+        // media type
+        CTMEDIATYPE                 m_cMediaType;
+#ifdef USING_KINECT_V2
+        // open the kinect v2
+        void open_v2() noexcept;
+        // shut down the kinect v2
+        void shut_down_v2() noexcept;
+        // dll for kinect v2
+        HMODULE                     m_hKinectV2 = nullptr;
+        // kinect v2
+        IKinectSensor*              m_pKinectV2 = nullptr;
+        // reader for color frame
+        IColorFrameReader*          m_pColorFrameReader = nullptr;
+#endif
+    };
+}
---
+++ Host/BasicHost/main.cpp
@@ -0,0 +1,74 @@
+﻿#include "BasicHost.h"
+
+// 创建Host
+extern "C" auto CreateHost(KineCT::ICTServer* pServer, KineCT::ICTHost** ppHost) noexcept ->HRESULT {
+    // 检测参数
+    if (!pServer || !ppHost) {
+        assert(!"bad arguments");
+        return E_INVALIDARG;
+    }
+
+}
+
+
+// CCTBasicHost 释放
+void KineCT::CCTBasicHost::Release() noexcept {
+    delete this;
+}
+
+// CCTBasicHost 构造函数
+KineCT::CCTBasicHost::CCTBasicHost(ICTServer* server) noexcept:m_pServer(server){
+    assert(server);
+    ::memset(&m_cMediaType, 0, sizeof(m_cMediaType));
+
+}
+
+// CCTBasicHost 析构函数
+KineCT::CCTBasicHost::~CCTBasicHost() noexcept {
+    // 关闭V2
+#ifdef USING_KINECT_V2
+    this->shut_down_v2();
+#endif
+}
+
+#ifdef USING_KINECT_V2
+// 打开KinectV2
+void KineCT::CCTBasicHost::open_v2() noexcept {
+    assert(!m_hKinectV2);
+    m_hKinectV2 = ::LoadLibraryW(L"Kinect20.dll");
+    // 检查
+    if (!m_hKinectV2) {
+        assert(!"m_hKinectV2 -> null");
+        ::MessageBoxW(nullptr, L"Cannot Find 'Kinect20.dll' 未找到", L"Error", MB_ICONERROR);
+        return;
+    }
+    // 获取默认Kinect
+    auto getDefaultKinectSensor = reinterpret_cast<decltype(&::GetDefaultKinectSensor)>(
+        ::GetProcAddress(m_hKinectV2, "GetDefaultKinectSensor")
+        );
+    auto hr = getDefaultKinectSensor(&m_pKinectV2);
+    // 绅士地打开Kinect
+    if (SUCCEEDED(hr)) {
+        hr = m_pKinectV2->Open();
+    }
+    // 检查错误
+    if (FAILED(hr)) {
+        ::MessageBoxW(nullptr, L"<KineCT::CCTBasicHost::open_v2> FAILED", L"Error", MB_ICONERROR);
+    }
+}
+
+// 关闭KinectV2
+void KineCT::CCTBasicHost::shut_down_v2() noexcept {
+    // 优雅地关闭Kinect
+    if (m_pKinectV2) {
+        m_pKinectV2->Close();
+        m_pKinectV2->Release();
+        m_pKinectV2 = nullptr;
+    }
+    // 释放Dll
+    if (m_hKinectV2) {
+        ::FreeLibrary(m_hKinectV2);
+        m_hKinectV2 = nullptr;
+    }
+}
+#endif
